80e6e6a87a724ce5e2f814100b65eb6c9d1bba38
Crystal <cforourk@umich.edu>
2026-06-25 09:49:27 -0400

    was joing to manually make the data but for now, letting faker fill it in

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,42 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
+import { createDepartment } from "#db/queries/departments";
+import { createProfessor } from "#db/queries/professors";
+import { createUser } from "#db/queries/users";
 
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
-async function seed() {}
+
+async function seed() {
+  await createUser("admin", "lincolnIsTheBestestEver9867543");
+
+  const departmentNames = [
+    "Computer Science",
+    "Mathematics",
+    "English Literature",
+    "Biology",
+    "History"
+  ];
+
+  for (const name of departmentNames) {
+    const department = await createDepartment(
+      name,
+      faker.internet.email(),
+      faker.lorem.paragraph(),
+      faker.image.url()
+    );
+
+    for (let i = 0; i < 4; i++) {
+      await createProfessor(
+        faker.person.fullName(),
+        faker.internet.email(),
+        faker.lorem.paragraph(),
+        faker.image.avatar(),
+        department.id
+      );
+    }
+  }
+}
